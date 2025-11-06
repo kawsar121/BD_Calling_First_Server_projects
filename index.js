748af2672d958ecd9eaa27a46e3d1eacc2ee5120
kawsar121 <tohidulislamkawsarbhuiyan2567gmail.com>
2025-11-06 19:57:32 +0600

    add server

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(
   cors({
     origin: ["http://localhost:5173",
-    "https://kb-cosmetics-products.netlify.app/"],
+    "https://kb-cosmetics-products.netlify.app"],
     credentials: true,
   })
 );
